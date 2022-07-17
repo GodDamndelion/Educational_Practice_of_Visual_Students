49f530110ff49172c55d3bbef0764803dbc3c1c8
GodDamndelion <bolshakov.pavel.alekseevich@yandex.ru>
2022-07-17 23:17:14 +0300

    Вот теперь точно готово

---
+++ Учебная Практика Визуал Студентики/Search_tree_2.h
@@ -0,0 +1,180 @@
+#pragma once
+
+namespace GroupWork {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Search_tree_2
+	/// </summary>
+	public ref class Search_tree_2 : public System::Windows::Forms::Form
+	{
+	public:
+		Search_tree_2(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Search_tree_2()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	public: System::Windows::Forms::Label^ label1;
+	public: System::Windows::Forms::Label^ label2;
+	public: System::Windows::Forms::Label^ label3;
+	public: System::Windows::Forms::Label^ label4;
+	public: System::Windows::Forms::Label^ label5;
+	public: System::Windows::Forms::Label^ label6;
+	public: System::Windows::Forms::Label^ label7;
+	protected:
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(597, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(17, 21);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"-";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label2->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(235, 69);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(17, 21);
+			this->label2->TabIndex = 2;
+			this->label2->Text = L"-";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label3->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(848, 69);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(17, 21);
+			this->label3->TabIndex = 3;
+			this->label3->Text = L"-";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label4->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(12, 150);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(17, 21);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"-";
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label5->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(359, 150);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(17, 21);
+			this->label5->TabIndex = 5;
+			this->label5->Text = L"-";
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label6->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label6->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label6->Location = System::Drawing::Point(735, 150);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(17, 21);
+			this->label6->TabIndex = 6;
+			this->label6->Text = L"-";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->BackColor = System::Drawing::SystemColors::ControlDark;
+			this->label7->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label7->Font = (gcnew System::Drawing::Font(L"Times New Roman", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label7->Location = System::Drawing::Point(1080, 150);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(17, 21);
+			this->label7->TabIndex = 7;
+			this->label7->Text = L"-";
+			// 
+			// Search_tree_2
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoScroll = true;
+			this->ClientSize = System::Drawing::Size(1327, 218);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label6);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"Search_tree_2";
+			this->Text = L"Search_tree_2";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+};
+}
